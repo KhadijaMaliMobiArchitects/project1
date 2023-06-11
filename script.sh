d3d767a928e00f4ddab0f2be850c5f91474253e5
KhadijaMaliMobiArchitects <128594978+KhadijaMaliMobiArchitects@users.noreply.github.com>
2023-06-11 05:24:16 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 # Write your commands here
 sudo su <<HERE
+docker stop ocprecette
+docker rm ocprecette
 docker pull ocpcontainerregistry.azurecr.io/ocp/recette:latest
 docker run -d --name ocprecette -p 80:3000 ocpcontainerregistry.azurecr.io/ocp/recette:latest
 HERE
